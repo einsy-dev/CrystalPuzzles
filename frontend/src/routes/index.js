97a00187af8f82598e4f745d2a7259f1193393db
Edgard <eins.yeager@gmail.com>
2024-05-03 19:15:48 +0500

    kinda work

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 import Root from '../pages/Root';
 import kidsRouter from './kids.router';
 import methodistRouter from './methodist.router';
@@ -6,7 +6,7 @@ import trainerRouter from './trainer.router';
 import checkInRouter from './check.in.router';
 
 export default function createRouter(role) {
-	return createBrowserRouter([
+	return createHashRouter([
 		{
 			path: '/',
 			element: <Root sidebar />,
